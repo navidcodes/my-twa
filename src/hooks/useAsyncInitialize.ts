04be6914ede9b54498cb23efc372497adee99a59
navidcodes <162269039+navidcodes@users.noreply.github.com>
2024-04-06 05:31:51 +0200

    first commit

---
+++ src/hooks/useAsyncInitialize.ts
@@ -0,0 +1,17 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useEffect, useState } from "react";
+
+export function useAsyncInitialize<T>(
+  func: () => Promise<T>,
+  deps: any[] = []
+) {
+  const [state, setState] = useState<T | undefined>();
+  useEffect(() => {
+    (async () => {
+      setState(await func());
+    })();
+  }, deps);
+
+  return state;
+}
